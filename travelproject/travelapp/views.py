7b231268fde74785cd73dbd5a6e086ca3ec36728
abc <abc@gmail.com>
2022-08-31 21:41:54 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place
+from .models import Peoples
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj2=Peoples.objects.all()
+
+    return render(request,"index.html",{'result':obj,'result2':obj2})
